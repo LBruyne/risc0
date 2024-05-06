d5e58d17f4cb2587024bc4846349469a1512bd62
Jizhen Wei <1477615023@qq.com>
2024-05-06 17:08:01 +0800

    Remove all files from branch

--- compact_proof/scripts/install_prover.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eoux
-
-docker build -f docker/prover.Dockerfile . -t risc0-groth16-prover
--- risc0-main/web/apps/benchmarks-and-reports/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    BENCHMARK_DATA: any;
-  }
-}
-
-export default global;
